f800748879d68d51ec1411fb9a09a3756f2c733b
my-name <my-email@address.com>
2025-03-02 07:36:05 +0900

    Added ExcelReaderWriter

---
+++ CSharp/ExcelReaderWriter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using OfficeOpenXml;
+
+class Program
+{
+    static void Main()
+    {
+        // Load the input.xlsx file
+        FileInfo inputFile = new FileInfo("input.xlsx");
+        FileInfo outputFile = new FileInfo("output.xlsx");
+
+        using (ExcelPackage package = new ExcelPackage(inputFile))
+        {
+            // Get the first worksheet
+            ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+            // Add a third column for the sums
+            worksheet.Cells[1, 3].Value = "y";
+
+            // Iterate through the rows and add the values
+            int rowCount = worksheet.Dimension.Rows;
+            for (int row = 2; row <= rowCount; row++)
+            {
+                double firstColumnValue = Convert.ToDouble(worksheet.Cells[row, 1].Value);
+                double secondColumnValue = Convert.ToDouble(worksheet.Cells[row, 2].Value);
+                worksheet.Cells[row, 3].Value = firstColumnValue + secondColumnValue;
+            }
+
+            // Save the worksheet to the output file
+            package.SaveAs(outputFile);
+        }
+
+        Console.WriteLine("Output file created successfully.");
+    }
+}
---
+++ FSharp/ExcelReaderWriter/Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+open System.IO
+open OfficeOpenXml
+
+
+let processExcelFile inputFilePath outputFilePath =
+    let inputFile = new FileInfo(inputFilePath)
+    let outputFile = new FileInfo(outputFilePath)
+
+    use package = new ExcelPackage(inputFile)
+    // Get the first worksheet
+    let worksheet = package.Workbook.Worksheets.[0]
+
+    // Add a third column for the sums
+    worksheet.Cells.[1, 3].Value <- "y"
+
+    // Iterate through the rows and add the values
+    let rowCount = worksheet.Dimension.Rows
+    for row in 2 .. rowCount do
+        let firstColumnValue = Convert.ToDouble(worksheet.Cells.[row, 1].Value)
+        let secondColumnValue = Convert.ToDouble(worksheet.Cells.[row, 2].Value)
+        worksheet.Cells.[row, 3].Value <- firstColumnValue + secondColumnValue
+
+    // Save the worksheet to the output file
+    package.SaveAs(outputFile)
+
+    printfn "Output file created successfully."
+
+
+[<EntryPoint>]
+let main argv =
+    let inputFilePath = "input.xlsx"
+    let outputFilePath = "output.xlsx"
+    processExcelFile inputFilePath outputFilePath
+    0
